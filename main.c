78c9527efd68f09d6ad31fb0f688c56c9386ee7f
jennisun <jsun20@stuy.edu>
2021-11-16 23:07:30 -0500

    added child process, removed duplicatetd child pid print statement

--- main.c
+++ main.c
@@ -4,30 +4,35 @@
 #include <time.h>
 #include <stdlib.h>
 
+int child() {
+  printf("Child process %d started\n", getpid());
+  int hold = rand() % 4 + 2;
+  sleep(hold);
+  printf("Child process %d finished\n", getpid());
+  return hold;
+}
+
 int main() {
-  printf("About to begin forking process\n");
+  printf("Beginning forking process\n");
 
   srand( time(NULL) );
-  int one = fork();
-  printf("Child PID: %d\n", getpid());
-  int two = fork();
-  printf("Child PID: %d\n", getpid());
+  int one, two;
 
-  if (one == -1 || two == -1) printf("Child process error\n");
+  one = fork();
+  if (one == -1) printf("Child process error\n");
+
+  if (one) {
+    two = fork();
+    if (two == -1) printf("Child process error\n");
+  }
+
   if (one && two) {
     int status;
-    int hold = rand() % 4 + 2;
-    sleep(hold);
     printf("Child process %d slept for %d and is now finished\n", wait(&status), WEXITSTATUS(status));
+    printf("Parent process finished\n");
     return 0;
   }
-
-  if (one || two) {
-    int hold = rand() % 4 + 2;
-    sleep(hold);
-    printf("Child process %d finished\n", getpid());
-    return hold;
-  }
-
-
+  else if (!one) return child();
+  else return child();
+  return 0;
 }
